4ffa4806923883db143c922c9be608db8f6f2035
joewuthrich <joerwuthrich@gmail.com>
2024-12-24 23:13:29 +1300

    RK: Added inital code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rank
+
+A basic website used to rank a list of items by providing a number of choices between two of the items on the list.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black w-full h-full flex justify-center items-center text-white">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
